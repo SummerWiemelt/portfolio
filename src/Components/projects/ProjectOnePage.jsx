38de5cc88504ebc6dbbd7649be47b8596168333b
Summer <summer.eisertw@gmail.com>
2020-03-05 11:07:07 -0700

    added Mistify

--- src/Components/projects/ProjectOnePage.jsx
+++ src/Components/projects/ProjectOnePage.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import desktop from '../../Assets/mistify-desktop.mp4';
+import mobile from '../../Assets/mistify-mobile.mp4';
 import './projectOne.scss';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
-import GifPlayer from 'react-gif-player';
+import ReactPlayer from 'react-player'
 
 class ProjectOne extends Component {
     render() {
@@ -16,34 +18,24 @@ class ProjectOne extends Component {
             </Row>
             <Row>
               <span className='project-subheader'>
-              Plant management website used to keep and track houseplant information. Built using React, Node.js, Express, Firestore authentication, Firebase (database), React router and others.
+              Plant management website used to keep and track houseplant information. Built using React, Redux, Firestore authentication, Firebase (database), React router, Node.js and Express. 
               </span>
               <span className='project-subheader'>
                 Click <a className='link' href="https://github.com/SummerWiemelt/Mistify">here</a> to see the source code on github
                 
               </span>
             </Row>
-            <Row className='gif-row'>
+            <Row className='video-row'>
               <span className='project-text'>Website design</span>
             </Row>
             <Row className='justify-content-center'>
-              <GifPlayer
-                // className='desktop-gif'
-                // gif={}
-                // still={}
-                // autoplay='true'
-              />
+            <ReactPlayer url={desktop} playing loop />
             </Row> 
-            <Row className='gif-row'>
+            <Row className='video-row'>
               <span className='project-text'>Responsive design works with all screen sizes</span>
             </Row>
-            <Row className='justify-content-center mobile-gif-parent'>
-              <GifPlayer
-                // className='mobile-gif'
-                // gif={}
-                // still={}
-                // autoplay='true'
-              />
+            <Row className='justify-content-center mobile-video-parent'>
+              <ReactPlayer url={mobile} playing loop />
             </Row>
           </Container>
         );
